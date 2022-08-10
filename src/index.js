cd0db4f8b449f3a23744587e86c493d3d502f012
you23 <73cocoa12@gmail.com>
2022-08-10 23:33:21 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import "./styles.css";
+/**
+ * const, let の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var value1 = "var1の変数";
+console.log(value1);
